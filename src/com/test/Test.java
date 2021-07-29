0b56105ecec00cb7d8abd8c7059080e69d40f0b0
AbdullahSDET <88009551+AbdullahSDET@users.noreply.github.com>
2021-07-29 19:41:37 -0400

    Update Test.java

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static void main (String args[]) {
 		
 		System.out.println("this is to update the git repo, test");
-		
+		System.out.println(" this should be listed back to the eclipse");
 		// TODO Auto-generated method stub
 
 	}
